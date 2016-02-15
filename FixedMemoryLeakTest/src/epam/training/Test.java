6438420234a08b0bf8f31f8f6466e7b41248df2f
Natalia Novikava <Natallia.Novikava>
2016-02-15 15:39:34 +0300

    This is a new commit for what I originally planned to be amended

---
+++ FixedMemoryLeakTest/src/epam/training/Test.java
@@ -0,0 +1,64 @@
+package epam.training;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Test {
+
+
+	public final static String fileName = "src/text.txt";
+	
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException
+	{
+		List<String> list = new ArrayList<String>();
+		String line = null;
+		String str = null;
+		BufferedReader bufferedReader = null;
+		
+		try {
+				for (int i = 0; i < 500; i++)
+				{   
+					bufferedReader = new BufferedReader(new FileReader(fileName));
+					while ((line = bufferedReader.readLine()) != null)
+					{  
+						str = new String(line.substring(0, 3));
+					    list.add(str);
+			    	}
+					if (bufferedReader != null)
+					{
+						bufferedReader.close();
+		       		}
+				}   
+		 
+		}
+		catch (FileNotFoundException e)
+		{
+             throw new IOException("File not found: " + fileName, e);
+        }
+		catch (IOException e)
+        {
+             throw new IOException("Unable to read file: " + fileName, e);
+        } finally {
+        	try {
+        		  if (bufferedReader != null)
+        		  {
+        		      bufferedReader.close();
+        		  }
+        	} catch (IOException e)
+        	{
+        		 throw new IOException("Unable to close file: " + fileName, e);
+            }
+        }
+	}
+}
+
+
